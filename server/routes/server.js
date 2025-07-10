5c0f036faec140692f5f1b78da7ae0ab9bc6dc40
tanishaaa17 <139698819+tanishaaa17@users.noreply.github.com>
2025-07-10 14:28:25 +0530

    Create server.js

---
+++ server/routes/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"));
+
+app.use('/api/events', require('./routes/events'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
