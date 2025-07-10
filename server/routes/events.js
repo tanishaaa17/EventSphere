0987fdbd2952c07ad2c337618ec320f684d0be59
tanishaaa17 <139698819+tanishaaa17@users.noreply.github.com>
2025-07-10 14:28:13 +0530

    Create events.js

---
+++ server/routes/events.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const Event = require('../models/Event');
+
+// Get all events
+router.get('/', async (req, res) => {
+  const events = await Event.find();
+  res.json(events);
+});
+
+module.exports = router;
